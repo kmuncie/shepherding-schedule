9ed6e2cabcc76855e9a5966858f0a16d019a206f
Kevin Muncie <kevin@kmuncie.com>
2023-04-19 16:36:20 -0400

    Add quasar ui framework

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+
+// A few examples for animations from Animate.css:
+// import @quasar/extras/animate/fadeIn.css
+// import @quasar/extras/animate/fadeOut.css
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +21,20 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+    /*
+    config: {
+      brand: {
+        // primary: '#e46262',
+        // ... or all other brand colors
+      },
+      notify: {...}, // default set of options for Notify Quasar plugin
+      loading: {...}, // default set of options for Loading Quasar plugin
+      loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+      // ..and many more (check Installation card on each Quasar component/directive/plugin)
+    }
+    */
+  })
 
 app.mount('#app')
