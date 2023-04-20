23eca9fe459b23ab20d5feb4a1bcf00df2a32b3e
Kevin Muncie <kevin@kmuncie.com>
2023-04-20 18:38:30 -0400

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 npm run build
-aws s3 sync public s3://kmuncie-site-prd/shepherding/ --delete --profile kmuncie
+aws s3 sync dist s3://kmuncie-site-prd/shepherding/ --delete --profile kmuncie
 aws cloudfront create-invalidation --distribution-id ENQKZZIYNE7H4 --paths "/shepherding/*" --profile kmuncie
 aws cloudfront list-invalidations --distribution-id ENQKZZIYNE7H4 --profile kmuncie
 
