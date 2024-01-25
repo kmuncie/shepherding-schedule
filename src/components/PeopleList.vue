a7eaf15097ec3737a3a2d9d2d4d6aab54e95b9d3
Kevin Muncie <kevin@kmuncie.com>
2024-01-25 16:29:50 -0500

    Update people list design

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -3,53 +3,52 @@
       <h2>People List</h2>
       <div class="card-container">
          <q-card bordered class="full-width shadow-up-2" v-for="person in sortedPeople" :key="person.id">
-            <q-card-section :class="{ 'text-deep-orange-6': person.role === 'shepherd', 'text-grey-5': person.departed }">
-               <h3 class="q-ma-none">{{ person.name }}</h3>
-               <p class="text-subtitle1 q-ma-none">
-                  {{ person.location }}
-                  <span v-if="person.role === 'shepherd'"> (Shepherd)</span>
-                  <q-chip outline :color="person.departed ? 'red' : 'grey-5'" :text-color="person.departed ? 'red' : 'grey-5'"
-                  size="xs" icon="directions_run"
-                  clickable @click="toggleDepartedStatus(person.id)">
-                  Departed
-               </q-chip>
-               </p>
+            <q-card-section :class="{
+               'q-card-section-flex': true,
+            }">
+               <div class="q-card-section-content" :class="{
+                  'text-deep-orange-6': person.role === 'shepherd',
+                  'text-grey-5': person.departed,
+                  'q-card-section-content': true,
+               }">
+                  <h3 class="q-ma-none">{{ person.name }}</h3>
+                  <p class="text-subtitle1 q-ma-none">
+                     {{ person.location }}
+                     <span v-if="person.role === 'shepherd'"> (Shepherd)</span>
+                     <q-chip outline :color="person.departed ? 'red' : 'grey-6'"
+                        :text-color="person.departed ? 'white' : 'grey-9'" size="xs" icon="directions_run" clickable
+                        @click="toggleDepartedStatus(person.id)">
+                        Departed
+                     </q-chip>
+                  </p>
+               </div>
+               <div class="q-card-section-actions">
+                  <q-btn flat round color="gray" icon="delete" v-if="!person.confirmRemoval"
+                     @click="toggleConfirmation(person)"></q-btn>
+                  <q-btn flat round color="red" icon="delete_forever" v-else @click="removePerson(person.id)"></q-btn>
+               </div>
             </q-card-section>
             <q-separator />
-            <q-expansion-item label="Meetings" icon="event_note" v-model="expanded">
-               <div v-for="meeting in person.meetings.filter(
-                  (m) => m.sheepId === person.id
-               )" :key="meeting.id">
-                  <div @click="() => {
+            <q-card-section>
+            <q-chip v-for="meeting in person.meetings.filter(
+               (m) => m.sheepId === person.id
+            )" :key="meeting.id" :class="[meeting.completed ? 'bg-green-2' : 'text-red-10']"
+               :removable="meeting.shepherdId === meeting.shepherdId" @click="() => {
                      meeting.completed = !meeting.completed;
-                     updateMeetingCompletion(
-                        meeting.id,
-                        meeting.completed
-                     );
-                  }
-                     ">
-                     <q-chip square :class="[
-                        meeting.completed ? 'bg-green-2' : 'text-red-10',
-                     ]" :removable="meeting.shepherdId === meeting.shepherdId"
-                        @remove="removeMeeting(meeting.shepherdId, meeting.id)">
-                        Q{{ meeting.quarter }} {{ meeting.year }} -
-                        {{ personNameById(meeting.shepherdId) }}
-                     </q-chip>
-                  </div>
-               </div>
-            </q-expansion-item>
-            <q-card-actions class="justify-around q-px-md">
-               <q-btn flat round color="gray" icon="delete" v-if="!person.confirmRemoval"
-                  @click="toggleConfirmation(person)"></q-btn>
-               <q-btn flat round color="red" icon="delete_forever" v-else @click="removePerson(person.id)"></q-btn>
-            </q-card-actions>
+                     updateMeetingCompletion(meeting.id, meeting.completed);
+                  }"
+                  @remove="removeMeeting(meeting.shepherdId, meeting.id)" clickable>
+               Q{{ meeting.quarter }} {{ meeting.year }} -
+               {{ personNameById(meeting.shepherdId) }}
+            </q-chip>
+            </q-card-section>
          </q-card>
       </div>
    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 import { usePeopleStore } from "@/stores/people";
 import { usePeopleById } from "@/composables/usePeopleById";
 import { useSortedPeople } from "@/composables/useSortedPeople";
@@ -83,7 +82,6 @@ export default defineComponent({
          useSortedPeople,
          updateMeetingCompletion,
          removeMeeting,
-         expanded: ref(true),
          toggleDepartedStatus,
       };
    },
@@ -99,7 +97,21 @@ export default defineComponent({
 
 @media (min-width: 1024px) {
    .card-container {
-      grid-template-columns: 1fr 1fr 1fr;
+      grid-template-columns: 1fr 1fr;
    }
+}
+
+.q-card-section-flex {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+}
+
+.q-card-section-content {
+   flex-grow: 1;
+}
+
+.q-card-section-actions {
+   /* Adjust as needed */
 }
 </style>
