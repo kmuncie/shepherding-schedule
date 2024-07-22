dd5b4ad5d243d73331b8fc6e8cd2758c3c938828
Kevin Muncie <kevin@kmuncie.com>
2024-07-22 19:26:53 -0400

    Remove old deploy script

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-npm run build
-aws s3 sync dist s3://kmuncie-site-prd/shepherding/ --delete --profile kmuncie
-aws cloudfront create-invalidation --distribution-id ENQKZZIYNE7H4 --paths "/shepherding/*" --profile kmuncie
-aws cloudfront list-invalidations --distribution-id ENQKZZIYNE7H4 --profile kmuncie
-
